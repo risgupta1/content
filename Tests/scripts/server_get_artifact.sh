320143518893eb41a582de33553a17269497a8c3
shaiyaakovi <yaakovi@demisto.com>
2018-03-27 15:21:53 +0300

    added debug prints

--- Tests/scripts/server_get_artifact.sh
+++ Tests/scripts/server_get_artifact.sh
@@ -5,8 +5,9 @@ ACCEPT_TYPE="Accept: application/json"
 SERVER_API_URI="https://circleci.com/api/v1/project/demisto/server"
 TOKEN_ATTR="circle-token=$SERVER_CI_TOKEN"
 
+echo "$TOKEN_ATTR"
 echo "Getting latest build num"
-TEMP=$(curl -s -H "$ACCEPT_TYPE" "$SERVER_API_URI/tree/master?limit=1&filter=successful&$TOKEN_ATTR")
+TEMP=$(curl -s -H "$ACCEPT_TYPE" "$SERVER_API_URI/tree/master?limit=10&filter=successful&$TOKEN_ATTR")
 
 # echo "$TEMP"
 ARTIFACT_BUILD_NUM=$(echo "$TEMP" | jq '.[0].build_num')
