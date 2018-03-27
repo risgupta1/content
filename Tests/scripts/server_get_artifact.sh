21cdf0db16a34d0ec920b47c3009a7cb13c943c0
shaiyaakovi <yaakovi@demisto.com>
2018-03-27 14:26:10 +0300

    added debug prints

--- Tests/scripts/server_get_artifact.sh
+++ Tests/scripts/server_get_artifact.sh
@@ -6,8 +6,12 @@ SERVER_API_URI="https://circleci.com/api/v1/project/demisto/server"
 TOKEN_ATTR="circle-token=$SERVER_CI_TOKEN"
 
 echo "Getting latest build num"
+TEMP=$(curl -s -H "$ACCEPT_TYPE" "$SERVER_API_URI/tree/master?limit=1&filter=successful&$TOKEN_ATTR")
 
-ARTIFACT_BUILD_NUM=$(curl -s -H "$ACCEPT_TYPE" "$SERVER_API_URI/tree/master?limit=1&filter=successful&$TOKEN_ATTR" | jq '.[0].build_num')
+echo "$TEMP"
+ARTIFACT_BUILD_NUM=$(echo "$TEMP" | jq '.[0].build_num')
+
+echo "in between curls"
 
 SERVER_DOWNLOAD_LINK=$(curl -s -H "$ACCEPT_TYPE" ${SERVER_API_URI}/${ARTIFACT_BUILD_NUM}/artifacts?${TOKEN_ATTR} | jq '.[].url' -r | grep demistoserver | grep /0/)
 
