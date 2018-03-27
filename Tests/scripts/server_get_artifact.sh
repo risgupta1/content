7345009c7bb1f91732058369d882dacc96346915
shaiyaakovi <yaakovi@demisto.com>
2018-03-27 16:29:53 +0300

    added debug prints

--- Tests/scripts/server_get_artifact.sh
+++ Tests/scripts/server_get_artifact.sh
@@ -5,18 +5,20 @@ ACCEPT_TYPE="Accept: application/json"
 SERVER_API_URI="https://circleci.com/api/v1/project/demisto/server"
 TOKEN_ATTR="circle-token=$SERVER_CI_TOKEN"
 
-echo "$TOKEN_ATTR"
 echo "Getting latest build num"
-TEMP=$(curl -s -H "$ACCEPT_TYPE" "$SERVER_API_URI/tree/master?limit=10&filter=successful&$TOKEN_ATTR")
+TEMP=$(curl -s -H "$ACCEPT_TYPE" "$SERVER_API_URI/tree/master?limit=5&filter=successful&$TOKEN_ATTR")
 
-# echo "$TEMP"
-ARTIFACT_BUILD_NUM=$(echo "$TEMP" | jq '.[0].build_num')
+ARTIFACTS_BUILD_NUM=
+for i in `seq 0 4`; do
+    if [[ $(echo "$TEMP" | jq ".[$i].build_parameters") == null ]]; then
+        ARTIFACTS_BUILD_NUM=$(echo "$TEMP" | jq ".[$i].build_num")
+        break
+    fi
+done
 
-echo "artifact id is $ARTIFACT_BUILD_NUM"
+# ARTIFACT_BUILD_NUM=$(echo "$TEMP" | jq '.[0].build_num')
 
-TEMP=$(curl -s -H "$ACCEPT_TYPE" ${SERVER_API_URI}/${ARTIFACT_BUILD_NUM}/artifacts?${TOKEN_ATTR} | jq '.[].url' -r)
-echo "\r\r\r $TEMP \r\r\r"
-SERVER_DOWNLOAD_LINK=$(echo "$TEMP" | grep demistoserver | grep /0/)
+SERVER_DOWNLOAD_LINK=$(curl -s -H "$ACCEPT_TYPE" ${SERVER_API_URI}/${ARTIFACT_BUILD_NUM}/artifacts?${TOKEN_ATTR} | jq '.[].url' -r | grep demistoserver | grep /0/)
 
 echo "Getting server artifact for build: ${ARTIFACT_BUILD_NUM}"
 echo "SERVER_DOWNLOAD_LINK = ${SERVER_DOWNLOAD_LINK}"
